5fb2150fea123d756ecaca42c0b29c9b080d8100
GitGinocchio <giuliotognetto2@gmail.com>
2023-04-09 07:12:41 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
-import json5
-
+import json
 
 class json_utils:
     def __init__(self,fp: str = None,*,indent: int = 3):
@@ -8,9 +7,9 @@ class json_utils:
     
     def content(self):
         with open(self.fp, 'r') as json_file:
-            content = json5.load(json_file)
+            content = json.load(json_file)
             return content
 
     def save_to_file(self,content,indent: int = 3):
         with open(self.fp, 'w') as json_file:
-            json5.dump(content,json_file,indent=indent)
+            json.dump(content,json_file,indent=indent)
