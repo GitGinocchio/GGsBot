d0b58149e53afd56aa827a0f7d1bec0ee657404a
GitGinocchio <giuliotognetto2@gmail.com>
2023-11-27 19:52:43 +0100

    Update RichPresence.py

--- cogs/RichPresence.py
+++ cogs/RichPresence.py
@@ -10,10 +10,9 @@ class RichPresence(commands.Cog):
     def __init__(self, bot : commands.Bot):
         super().__init__()
         self.bot = bot
-        self.RPC
-        RPC = Presence(client_id=str(CLIENT_ID))
-        RPC.connect()
-        RPC.update(details="Sto programmando", state="Utilizzando discord.py", large_image="large_image_key")
+        self.RPC = Presence(client_id=str(CLIENT_ID))
+        self.RPC.connect()
+        self.RPC.update(details="Sto programmando", state="Utilizzando discord.py", large_image="large_image_key")
 
 def setup(bot):
     bot.add_cog(RichPresence(bot))
