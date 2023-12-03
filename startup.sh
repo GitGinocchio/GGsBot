719bef4edbe6f8c7d78cc96711a58aa0505b332f
GitGinocchio <giuliotognetto2@gmail.com>
2023-12-03 03:35:33 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 if [ -d .git ] && [ 1 = "1" ]; then
-    #git config --global credential.helper store
-    #git config --global pull.ff only  # Imposta la strategia di merge predefinita a fast-forward only
+    #git config credential.helper store
+    #git config pull.rebase false  # Imposta la strategia di merge predefinita a merge
+    #git config pull.rebase true  # Imposta la strategia di merge predefinita a rebase
+    #git config pull.ff only  # Imposta la strategia di merge predefinita a fast-forward only
     git pull
 fi
 
