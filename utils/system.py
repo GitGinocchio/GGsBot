569852433709d06a83023c66af62c4c36227435e
GitGinocchio <giuliotognetto2@gmail.com>
2024-04-04 18:34:19 +0200

    Update system.py

--- utils/system.py
+++ utils/system.py
@@ -1,8 +1,8 @@
+from typing import Literal
 import platform
 
-
-def getos():
+def getos() -> Literal['Windows','Linux']:
     return platform.platform().split('-')[0]
 
-def getarch():
+def getarch() -> Literal['x64','x32']:
     return 'x64' if '64' in platform.architecture()[0] else 'x32'
