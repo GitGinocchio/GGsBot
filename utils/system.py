5460fbff3b9bf7e9413ff88a6fa679659a2eecde
GitGinocchio <giuliotognetto2@gmail.com>
2024-04-04 20:23:38 +0200

    Update system.py

--- utils/system.py
+++ utils/system.py
@@ -4,7 +4,7 @@ import platform
 
 logger = getlogger()
 
-OS = platform.platform().split('-')[0]
+OS = platform.system()
 
 ARCH = 'x64' if '64' in platform.architecture()[0] else 'x32'
 
@@ -12,7 +12,6 @@ def getsysteminfo():
     logger.debug(f'Architecture: {platform.architecture()}')
     logger.debug(f'Platform: {platform.platform()}')
     logger.debug(f'Machine: {platform.machine()}')
-    logger.debug(f'Uname: {platform.uname()}')
     logger.debug(f'Node: {platform.node()}')
     logger.debug(f'System: {platform.system()}')
     logger.debug(f'Python Version: {platform.python_version()}')
