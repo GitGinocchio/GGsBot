6537fffa520a15f987ac648ada727e5ec3f6dd36
GitGinocchio <giuliotognetto2@gmail.com>
2024-04-04 20:20:03 +0200

    debug changes

--- commands/music/MusicCommands.py
+++ commands/music/MusicCommands.py
@@ -54,10 +54,10 @@ class MusicCommands(commands.Cog):
             assert not interaction.guild.voice_client, f'{interaction.user.mention} I am currently in a voice channel!'
 
             await interaction.user.voice.channel.connect()
-            await interaction.response.send_message(f"{interaction.user.mention} I joined your voice channel!", ephemeral=True, delete_after=2.5)
+            await interaction.response.send_message(f"{interaction.user.mention} I joined your voice channel!", ephemeral=True, delete_after=5.0)
 
         except AssertionError as e:
-            await interaction.response.send_message(e,ephemeral=True,delete_after=5)
+            await interaction.response.send_message(e,ephemeral=True,delete_after=5.0)
         
     @nextcord.slash_command('music_leave',"The bot will leave your vocal channel!",default_member_permissions=8,dm_permission=False)
     async def leave(self, interaction : nextcord.Interaction):
--- utils/system.py
+++ utils/system.py
@@ -1,6 +1,20 @@
 from typing import Literal
+from utils.terminal import getlogger
 import platform
+
+logger = getlogger()
 
 OS = platform.platform().split('-')[0]
 
 ARCH = 'x64' if '64' in platform.architecture()[0] else 'x32'
+
+def getsysteminfo():
+    logger.debug(f'Architecture: {platform.architecture()}')
+    logger.debug(f'Platform: {platform.platform()}')
+    logger.debug(f'Machine: {platform.machine()}')
+    logger.debug(f'Uname: {platform.uname()}')
+    logger.debug(f'Node: {platform.node()}')
+    logger.debug(f'System: {platform.system()}')
+    logger.debug(f'Python Version: {platform.python_version()}')
+    logger.debug(f'Python Build: {platform.python_build()}')
+    logger.debug(f'Python Revision: {platform.python_revision()}')
