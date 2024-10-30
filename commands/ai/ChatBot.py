26dc285e70ae91cd3ae333f99e3ce7e36246268b
GitGinocchio <giuliotognetto2@gmail.com>
2024-10-30 22:48:38 +0100

    minor changes

--- commands/ai/ChatBot.py
+++ commands/ai/ChatBot.py
@@ -42,22 +42,6 @@ txt2txt_models = [
     '@cf/tinyllama/tinyllama-1.1b-chat-v1.0',
 ]
 
-txt2img_models = [
-    "@cf/black-forest-labs/flux-1-schnell",
-]
-
-img2img_models = [
-    "@cf/runwayml/stable-diffusion-v1-5-inpainting",
-    "@cf/runwayml/stable-diffusion-v1-5-img2img",
-    "@cf/bytedance/stable-diffusion-xl-lightning",
-    "@cf/stabilityai/stable-diffusion-xl-base-1.0"
-]
-
-img2txt_models = [
-    "@cf/llava-hf/llava-1.5-7b-hf",
-    "@cf/unum/uform-gen2-qwen-500m"
-]
-
 permissions = Permissions(
     use_slash_commands=True
     #administrator=True
@@ -68,68 +52,10 @@ class ChatBot(commands.Cog):
         self.dirfmt = './data/guilds/{guild_id}/commands.ai.ChatBot'
         self.bot = bot
 
-    @slash_command('ai',"An AI chatbot powered by LLMs (Large Language Models)",default_member_permissions=permissions,dm_permission=False)
-    async def ai(self, interaction : Interaction): pass
-
-    @ai.subcommand('chat',"Set of commands to manage Ai chats")
+    @slash_command('chat',"An AI chatbot powered by LLMs (Large Language Models)",default_member_permissions=permissions,dm_permission=False)
     async def chat(self, interaction : Interaction): pass
 
-    @chat.subcommand('new',"Create a chat with GG'sBot Ai")
-    async def newchat(self, 
-                      interaction : Interaction,
-                      public : bool = SlashOption('public','Whether the chat between you and the bot should be public or private',required=True,default=False),
-                      aimodel : str = SlashOption("aimodel","Choose the Artificial Intelligence model you want to use",required=True,choices=txt2txt_models,default='@cf/meta/llama-3-8b-instruct'),
-                      template : str | None = SlashOption("template","Templates are used to get more specific answers for the type of context you want to get.",required=False,choices=templates,default=None),
-                      tags: str = SlashOption("tags","Write separated comma Tags used to get more specific answers",required=False,default=''),
-                    ):
-        await interaction.response.defer(ephemeral=True)
-        workingdir = self.dirfmt.format(guild_id=interaction.guild.id)
-        try:
-            if not os.path.exists(f'{workingdir}/config.json'): raise ExtensionException("Not Configured")
-
-            file = JsonFile(f'{workingdir}/config.json')
-
-            if not interaction.channel.id == int(file['aichatbot-text-channel']): raise SlashCommandException("Invalid Channel")
-
-            thread : Thread = await interaction.channel.create_thread(
-                                                            name="New Chat",
-                                                            reason=f'<@{interaction.user.id}> creates a GG\'Bot AI chat',
-                                                            type=ChannelType.public_thread if public else ChannelType.private_thread
-                                                            )
-            await thread.edit(slowmode_delay=file['aichatbot-chat-delay'])
-            await thread.add_user(interaction.user)
-
-            file['threads'][str(thread.id)] = {
-                "template" : template,
-                "aimodel" : aimodel,
-                "creator" : interaction.user.name,
-                "creator-mention" : interaction.user.mention,
-                "tags" : tags
-                }
-        except (ExtensionException,SlashCommandException) as e:
-            await interaction.followup.send(embed=e.asEmbed())
-        else:
-            await interaction.followup.send("Chat created successfully")
-
-    @chat.subcommand('del',"Delete a chat with GG'sBot Ai")
-    async def delchat(self, interaction : Interaction):
-        try:
-            await interaction.response.defer(ephemeral=True)
-
-            if not str(interaction.channel.id) in file['threads']: raise SlashCommandException("Invalid Channel")
-            
-            workingdir = self.dirfmt.format(guild_id=interaction.guild.id)
-            if not os.path.exists(f'{workingdir}/config.json'): raise ExtensionException("Not Configured")
-
-            file = JsonFile(f'{workingdir}/config.json')
-
-            await interaction.channel.delete()
-            file['threads'].pop(str(interaction.channel.id))
-        except (SlashCommandException, ExtensionException) as e:
-            await interaction.followup.send(embed=e.asEmbed())
-            logger.log(e)
-
-    @ai.subcommand('ask',description="Ask a question to GG'sBot AI")
+    @slash_command('ask',description="Ask a question to GG'sBot AI",default_member_permissions=permissions,dm_permission=False)
     async def ask(self,
                   interaction : Interaction,
                   prompt : str = SlashOption("prompt","The question you want to ask",required=True),
@@ -189,119 +115,60 @@ class ChatBot(commands.Cog):
             await interaction.followup.send(embed=e.asEmbed())
             logger.error(e)
 
-    @ai.subcommand('image', "Set of commands to manage and create Ai generated images")
-    async def image(self, interaction : Interaction): pass
-
-    @image.subcommand('fromtext', "Generate an image from a prompt")
-    async def fromtext(self, 
-                       interaction : Interaction,
-                       prompt : str =  SlashOption(description="A text description of the image you want to generate",required=True,min_length=1),
-                       model : str = SlashOption(description="Model to be used for generating the image",required=False,choices=txt2img_models,default=txt2img_models[0]),
-                       steps : int = SlashOption(description="Number of diffusion steps, higher values can improve quality but take longer (min: 1 max: 8 def:4)",required=False,default=4, min_value=1, max_value=8),
-                       ephemeral : bool = SlashOption(description="Whether the response should be ephemeral or not",required=False,default=True),
-                       spoiler : bool = SlashOption(description="Whether the generated image should be a spoiler or not",required=False,default=False),
-                       ):
+    @chat.subcommand('new',"Create a chat with GG'sBot Ai")
+    async def newchat(self, 
+                      interaction : Interaction,
+                      public : bool = SlashOption('public','Whether the chat between you and the bot should be public or private',required=True,default=False),
+                      aimodel : str = SlashOption("aimodel","Choose the Artificial Intelligence model you want to use",required=True,choices=txt2txt_models,default=txt2txt_models[0]),
+                      template : str | None = SlashOption("template","Templates are used to get more specific answers for the type of context you want to get.",required=False,choices=templates,default=None),
+                      tags: str = SlashOption("tags","Write separated comma Tags used to get more specific answers",required=False,default=''),
+                    ):
+        await interaction.response.defer(ephemeral=True)
+        workingdir = self.dirfmt.format(guild_id=interaction.guild.id)
         try:
-            await interaction.response.defer(ephemeral=ephemeral)
+            if not os.path.exists(f'{workingdir}/config.json'): raise ExtensionException("Not Configured")
 
-            url = f"https://api.cloudflare.com/client/v4/accounts/{os.environ['CLOUDFLARE_ACCOUNT_ID']}/ai/run/{model}"
-            headers = { "Authorization": f"Bearer {os.environ['CLOUDFLARE_API_KEY']}", 'Content-Type': 'application/json' }
-            data = { "prompt" : prompt, "steps" : steps}
+            file = JsonFile(f'{workingdir}/config.json')
 
-            content = requests.post(url, headers=headers, json=data).json()
+            if not interaction.channel.id == int(file['aichatbot-text-channel']): raise SlashCommandException("Invalid Channel")
 
-            if not content['success']: raise CloudFlareAIException(code=content['errors'][0]['code'])
+            thread : Thread = await interaction.channel.create_thread(
+                                                            name="New Chat",
+                                                            reason=f'<@{interaction.user.id}> creates a GG\'Bot AI chat',
+                                                            type=ChannelType.public_thread if public else ChannelType.private_thread
+                                                            )
+            await thread.edit(slowmode_delay=file['aichatbot-chat-delay'])
+            await thread.add_user(interaction.user)
 
-            b64image = content['result']['image']
-
-            embed = Embed(
-                title=f"Image generated by {interaction.user.name}",
-                color=Color.green(),
-                timestamp=datetime.now(timezone.utc)
-            )
-            embed.add_field(name="Model:", value=f"**{model}**", inline=True)
-            embed.add_field(name="Diffusion Steps: ", value=steps, inline=True)
-            embed.add_field(name="Prompt:", value=prompt[:1021] + '...' if len(prompt) > 1024 else prompt, inline=False)
-            embed.set_footer(
-                text=f"Generated by {self.bot.user.name} powered by Cloudflare Ai Workers", 
-                icon_url="https://static-00.iconduck.com/assets.00/cloudflare-icon-512x512-c1lpcyo0.png"
-            )
-            embed.set_author(name=interaction.user.name,icon_url=interaction.user.avatar.url if interaction.user.avatar else interaction.user.default_avatar.url)
-
-            await interaction.followup.send(embed=embed, file=File(io.BytesIO(base64.b64decode(b64image)), filename="image.png",description=f"Image generated by GGsBot", spoiler=spoiler))
-        except CloudFlareAIException as e:
+            file['threads'][str(thread.id)] = {
+                "template" : template,
+                "aimodel" : aimodel,
+                "creator" : interaction.user.name,
+                "creator-mention" : interaction.user.mention,
+                "tags" : tags
+                }
+        except (ExtensionException,SlashCommandException) as e:
             await interaction.followup.send(embed=e.asEmbed())
-            logger.error(e)
+        else:
+            await interaction.followup.send("Chat created successfully")
 
-    """
-    @image.subcommand('fromimage', "Generate an image from another image and/or from a prompt")
-    async def fromimage(self,
-                       interaction : Interaction,
-                       image : Attachment = SlashOption("image",description="The image to be used as a base for the generated image",required=True),
-                       prompt : str =  SlashOption(description="A text description of the image you want to generate",required=True,min_length=1),
-                       negativeprompt : str = SlashOption(description="Text describing elements to avoid in the generated image",required=False),
-                       height : int = SlashOption(description="Height of the generated image. Default is 512",required=False,default=512, min_value=256, max_value=2048),
-                       width : int = SlashOption(description="Width of the generated image. Default is 512",required=False,default=512,min_value=256,max_value=2048),
-                       steps : int = SlashOption(description="The number of diffusion steps; higher values can improve quality but take longer",required=False,default=20, min_value=1, max_value=20),
-                       guidance : float = SlashOption(description="Controls how closely the generated image should adhere to the prompt",required=False,default=7.5, min_value=0.0,max_value=10),
-                       seed : int = SlashOption(description="Random seed for reproducibility of the image generation",required=False, default=None),
-                       model : str = SlashOption(description="The model to be used for generating the image",required=False,choices=img2img_models,default=img2img_models[0]),
-                       ephemeral : bool = SlashOption(description="Whether the response should be ephemeral or not",required=False,default=True),
-                       ):
+    @chat.subcommand('del',"Delete a chat with GG'sBot Ai")
+    async def delchat(self, interaction : Interaction):
         try:
-            await interaction.response.defer(ephemeral=ephemeral)
+            await interaction.response.defer(ephemeral=True)
 
-            print(image.content_type)
-            assert image.content_type == 'image/png', "Image must be a png image"
+            if not str(interaction.channel.id) in file['threads']: raise SlashCommandException("Invalid Channel")
+            
+            workingdir = self.dirfmt.format(guild_id=interaction.guild.id)
+            if not os.path.exists(f'{workingdir}/config.json'): raise ExtensionException("Not Configured")
 
-            image_bytes = await image.read()
-            PIL_image = Image.open(io.BytesIO(image_bytes))
+            file = JsonFile(f'{workingdir}/config.json')
 
-            mask_array = []
-            image_array = []
-
-            url = f"https://api.cloudflare.com/client/v4/accounts/{os.environ['CLOUDFLARE_ACCOUNT_ID']}/ai/run/{model}"
-            headers = {
-                "Authorization": f"Bearer {os.environ['CLOUDFLARE_API_KEY']}",
-                'Content-Type': 'application/json'
-            }
-
-            data = {
-                "prompt": prompt,
-                #"guidance": guidance,
-                #"num_steps": steps,
-                "mask" : mask_array,
-                "image" : image_array,
-                "image_b64": base64.b64encode(image_bytes).decode(),
-                "width": width,
-                "height": height
-            }
-            if negativeprompt: data["negative_prompt"] = negativeprompt
-            if seed: data['seed'] = seed
-
-            print(data.keys())
-
-            content = requests.post(url, headers=headers, json=data).json()
-            assert content['success'], f"An error occurred in cloudflare API (code:{content['errors'][0]['code']}): {content['errors'][0]['message']}"
-
-            b64image = content['result']['image']
-
-            await interaction.followup.send(content=f"{prompt}:",file=File(io.BytesIO(base64.b64decode(b64image)),'image.png',description=f"Image generated by GGsBot"))
-
-        except AssertionError as e:
-            logger.error(e)
-    """
-
-    """
-    @image.subcommand('describe', "Describes an image")
-    async def describe(self, 
-                       interaction  : Interaction
-                       ):
-        try:
-            await interaction.response.defer(ephemeral=ephemeral)
-        except AssertionError as e:
-            logger.error(e)
-    """
+            await interaction.channel.delete()
+            file['threads'].pop(str(interaction.channel.id))
+        except (SlashCommandException, ExtensionException) as e:
+            await interaction.followup.send(embed=e.asEmbed())
+            logger.log(e)
 
     @commands.Cog.listener()
     async def on_message(self, message : Message):
