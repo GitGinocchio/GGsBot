e2288e3c4eef0bdff3458307beb02ea7185f652a
GitGinocchio <giuliotognetto2@gmail.com>
2025-01-04 19:55:04 +0100

    Updated logging module with date-formatted filename.

--- src/utils/terminal.py
+++ src/utils/terminal.py
@@ -1,6 +1,7 @@
 from utils.config import config
 from colorama import Fore as F
 from datetime import datetime
+from enum import Enum
 import subprocess
 import inspect
 import logging
@@ -17,41 +18,50 @@ def erase():
     sys.stdout.write('\033[F')
     sys.stdout.write('\033[K')
 
-levels = {
-    "DEBUG"      :   (logging.DEBUG,      F.GREEN          ),
-    "INFO"       :   (logging.INFO,       F.WHITE          ),
-    "WARNING"    :   (logging.WARNING,    F.LIGHTYELLOW_EX ),
-    "ERROR"      :   (logging.ERROR,      F.YELLOW         ),
-    "CRITICAL"   :   (logging.CRITICAL,   F.LIGHTRED_EX    ),
-    "FATAL"      :   (logging.FATAL,      F.RED            )
-}
+class Level(Enum):
+    DEBUG    = (logging.DEBUG,      F.GREEN          )
+    INFO     = (logging.INFO,       F.WHITE          )
+    WARNING  = (logging.WARNING,    F.LIGHTYELLOW_EX )
+    ERROR    = (logging.ERROR,      F.YELLOW         )
+    CRITICAL = (logging.CRITICAL,   F.LIGHTRED_EX    )
+    FATAL    = (logging.FATAL,      F.RED            )
+
 
 class CustomColorsFormatter(logging.Formatter):
     def format(self, record : logging.LogRecord):
-        color = levels.get(record.levelname, (logging.INFO, F.WHITE))
-        record.name = f"{F.LIGHTMAGENTA_EX}[{record.name}]{F.RESET}"
-        record.msg = f": {color[1]}{record.msg}{F.RESET}"
-        record.levelname = f"{color[1]}[{record.levelname}]{F.RESET}"
+        level = Level[record.levelname] if record.levelname in Level.__members__ else Level.INFO
+        record.colored_name = f"{F.LIGHTMAGENTA_EX}[{record.name}]{F.RESET}"
+        record.colored_msg = f": {level.value[1]}{record.msg}{F.RESET}"
+        record.colored_levelname = f"{level.value[1]}[{record.levelname}]{F.RESET}"
 
         return super().format(record)
 
-formatter = CustomColorsFormatter(
-    '[%(asctime)s] %(name)s %(levelname)s %(message)s',
-    datefmt=config["logger"]["datefmt"])
+
+stream_formatter = CustomColorsFormatter(
+    '[%(asctime)s] %(colored_name)s %(colored_levelname)s %(colored_msg)s',
+    datefmt=config["logger"]["datefmt"]
+)
+file_formatter = logging.Formatter(
+    '[%(asctime)s] [%(name)s] [%(levelname)s] : %(message)s',
+    datefmt=config["logger"]["datefmt"]
+)
 
 stream = logging.StreamHandler(sys.stdout)
-stream.setFormatter(formatter)
+stream.setFormatter(stream_formatter)
 
 if config["logger"]["tofile"]:
-    logfile = logging.FileHandler("{}/{}".format(
+    logfile = logging.FileHandler(r"{}//{}.log".format(
         config["logger"]['dir'],
-        datetime.now().strftime(config["logger"]["datefmt"])))
-    logfile.setFormatter(formatter)
+        datetime.now().strftime(config["logger"]["filename_datefmt"])),
+        encoding="utf-8"
+    )
+    logfile.setFormatter(file_formatter)
 
-level = levels.get(config["logger"]["level"], logging.INFO)
+
+default_level = Level[str_lvl] if (str_lvl:=config["logger"]["level"]) in Level.__members__ else Level.INFO
 
 
-def getlogger(name : str = None) -> logging.Logger:
+def getlogger(name : str = None, level : Level = None) -> logging.Logger:
     if name is None:
         match = re.match(r".*[\\/](.+?)(\.[^.]*$|$)", inspect.stack()[1].filename)
 
@@ -62,10 +72,10 @@ def getlogger(name : str = None) -> logging.Logger:
 
     logger = logging.getLogger(name)
 
-    if isinstance(level, tuple):
+    if level is not None:
         logger.setLevel(level[0])
     else:
-        logger.setLevel(level)
+        logger.setLevel(default_level.value[0])
 
     logger.addHandler(stream)
 
